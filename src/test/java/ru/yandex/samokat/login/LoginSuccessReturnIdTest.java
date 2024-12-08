156e833da976a573d37a0fe42f02dff6ae48c74d
Dmitry Shikov <shikovspace@yandex.ru>
2024-12-08 14:28:19 +0300

    add allure report

---
+++ src/test/java/ru/yandex/samokat/login/LoginSuccessReturnIdTest.java
@@ -0,0 +1,46 @@
+package java.ru.yandex.samokat.login;
+
+import com.github.javafaker.Faker;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.yandex.model.CourierAccount;
+import ru.yandex.samokat.steps.Steps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LoginSuccessReturnIdTest {
+
+    private final Faker faker = new Faker(new Locale("en"));
+    private final Steps steps = new Steps();
+    private CourierAccount account;
+    private List<CourierAccount> testData;
+
+    @Before
+    public void setUp() {
+        testData = new ArrayList<>();
+        account = new CourierAccount(faker.funnyName().name(), faker.internet().password(), faker.name().firstName());
+        testData.add(account);
+    }
+
+    @Test
+    @DisplayName("Успешный запрос возвращает id")
+    public void loginSuccessReturnId() {
+        steps.create(account);
+        ValidatableResponse response = steps.login(account);
+        assertThat("Успешный запрос возвращает \"id\": int", response.extract().body().jsonPath().
+                getInt("id"), notNullValue());
+    }
+
+    @After
+    public void cleanUp() {
+        steps.delete(testData);
+    }
+}
---
+++ src/test/java/ru/yandex/samokat/order/OrdersTest.java
@@ -0,0 +1,23 @@
+package java.ru.yandex.samokat.order;
+
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.junit.Test;
+import ru.yandex.model.Orders;
+import ru.yandex.samokat.steps.Steps;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class OrdersTest {
+    private final Steps steps = new Steps();
+
+    @Test
+    @DisplayName("Список заказов - В тело ответа возвращается список заказов")
+    public void ordersReturnsOrderList() {
+        Response response = steps.orders();
+        assertThat("В тело ответа возвращается список заказов",
+                response.as(Orders.class).getOrders(),
+                notNullValue());
+    }
+}
